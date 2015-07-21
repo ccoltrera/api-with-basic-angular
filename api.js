b70315c8d3efe03e703510c5f87f4c2b2cbc8885
Colin Coltrera <c.coltrera@gmail.com>
2015-07-21 09:33:41 -0700

    Divide functionality into api.js / router.js / handler.js. Give handlers descriptive names.

--- api.js
+++ api.js
@@ -7,101 +7,18 @@ var app = express();
 app.use(bodyParser.json());
 
 var db = require(__dirname + "/lib/db");
-var Entry = require(__dirname + "/lib/model");
+var router = require(__dirname + "/lib/router");
 
 app.route("/api/entry")
-  .post(function(req, res) {
-    //Check if entry a duplicate by title
-    Entry.find({title: req.body.title}, function(err, entries) {
-
-      if (entries.length === 0) {
-        var reqEntry = new Entry(req.body);
-        reqEntry.save(function(err, reqEntry) {
-          if (err) {
-            console.log(err);
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(200);
-          }
-        });
-      } else {
-        res.sendStatus(409);
-      }
-    });
-  });
+  .post(router.entry.POST);
 
 app.route("/api/entry/:title")
-  .get(function(req, res) {
-    //Decode the URI encoded title
-    var entryTitle = decodeURIComponent(req.params.title);
-    Entry.find({title: entryTitle}, function(err, entries) {
-      if (entries.length === 0) {
-        res.sendStatus(404);
-      } else {
-        res.json(entries[0]);
-      }
-    });
-  })
-  .put(function(req, res) {
-    //Decode the URI encoded title
-    var entryTitle = decodeURIComponent(req.params.title);
-    Entry.find({title: entryTitle}, function(err, entries) {
-      if (entries.length === 0) {
-        var reqEntry = new Entry(req.body);
-        reqEntry.save(function(err, reqEntry) {
-          if (err) {
-            console.log(err);
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(201);
-          }
-        });
-      } else {
-        Entry.findOneAndUpdate({title: entryTitle}, req.body, {overwrite: true}, function(err) {
-          if (err) {
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(200);
-          }
-        });
-      }
-    });
-  })
-  .delete(function(req, res) {
-    var entryTitle = decodeURIComponent(req.params.title);
-    Entry.find({title: entryTitle}, function(err, entries) {
-      if (entries.length === 0) {
-        res.sendStatus(400);
-      } else {
-        Entry.remove({title: entryTitle}, function(err) {
-          if (err) {
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(200);
-          }
-        });
-      }
-    });
-  });
+  .get(router.entry.title.GET)
+  .put(router.entry.title.PUT)
+  .delete(router.entry.title.DELETE);
 
 app.route("/api/vote/:title")
-  .patch(function(req, res) {
-    var entryTitle = decodeURIComponent(req.params.title);
-    Entry.find({title: entryTitle}, function(err, entries) {
-      if (entries.length === 0) {
-        res.sendStatus(400);
-      } else {
-        Entry.update({title: entryTitle}, {$inc: {votes: 1}}, function(err) {
-          if (err) {
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(200);
-          }
-        });
-      }
-    });
-
-  });
+  .patch(router.vote.title.PATCH);
 
 var apiServer = app.listen(8080, function() {
   console.log("Server running on http://localhost:8080");
