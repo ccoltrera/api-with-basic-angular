3707e7ab837b358fc4920e8503180a866021a56e
Colin Coltrera <c.coltrera@gmail.com>
2015-08-10 11:10:36 -0700

    Explicate the scope through naming.

--- test/karma_tests/blog_controller_test.js
+++ test/karma_tests/blog_controller_test.js
@@ -5,19 +5,19 @@ require("angular-mocks");
 describe("blog controller", function() {
   var $ControllerConstructor;
   var $httpBackend;
-  var $scope;
+  var blogScope;
 
   beforeEach(angular.mock.module("blogApp"));
 
   beforeEach(angular.mock.inject(function(_$rootScope_, _$controller_) {
-    $scope = _$rootScope_.$new();
+    blogScope = _$rootScope_.$new();
     $ControllerConstructor = _$controller_;
   }));
 
   describe("REST functionality", function() {
     beforeEach(angular.mock.inject(function(_$httpBackend_, _$rootScope_) {
       $httpBackend = _$httpBackend_;
-      $ControllerConstructor("blogController", {$scope: $scope});
+      $ControllerConstructor("blogController", {$scope: blogScope});
     }));
 
     afterEach(function() {
@@ -33,15 +33,15 @@ describe("blog controller", function() {
           votes: 0,
           _id: 1
         }]);
-        $scope.getAll();
+        blogScope.getAll();
         $httpBackend.flush();
-        expect($scope.entries.length).toBe(1);
-        expect($scope.entries[0].title).toBe("what a title!");
-        expect($scope.entries[0]._id).toBe(1);
+        expect(blogScope.entries.length).toBe(1);
+        expect(blogScope.entries[0].title).toBe("what a title!");
+        expect(blogScope.entries[0]._id).toBe(1);
     });
 
       it("should make a get request and set var user to '' when logout is called", function() {
-        $scope.user = "reader";
+        blogScope.user = "reader";
         $httpBackend.expectGET("/api/entries").respond(200, [{
           datePosted: new Date,
           title: "what a title!",
@@ -49,19 +49,19 @@ describe("blog controller", function() {
           votes: 0,
           _id: 1
         }]);
-        $scope.logout();
+        blogScope.logout();
         $httpBackend.flush();
-        expect($scope.entries.length).toBe(1);
-        expect($scope.entries[0].title).toBe("what a title!");
-        expect($scope.entries[0]._id).toBe(1);
-        expect($scope.user).toBe("");
+        expect(blogScope.entries.length).toBe(1);
+        expect(blogScope.entries[0].title).toBe("what a title!");
+        expect(blogScope.entries[0]._id).toBe(1);
+        expect(blogScope.user).toBe("");
     });
 
   });
 
   describe("Non-REST functionality", function() {
     beforeEach(angular.mock.inject(function(_$rootScope_) {
-      $ControllerConstructor("blogController", {$scope: $scope});
+      $ControllerConstructor("blogController", {$scope: blogScope});
     }));
 
     afterEach(function() {
@@ -70,13 +70,13 @@ describe("blog controller", function() {
     });
 
     it("should set var user to 'reader' when reader is called", function() {
-      $scope.reader();
-      expect($scope.user).toBe("reader");
+      blogScope.reader();
+      expect(blogScope.user).toBe("reader");
     })
 
     it("should set var user to 'writer' when writer is called", function() {
-      $scope.writer();
-      expect($scope.user).toBe("writer");
+      blogScope.writer();
+      expect(blogScope.user).toBe("writer");
     })
 
   });
