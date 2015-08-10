e6c617943d5257480612cff17986def0857ef363
Colin Coltrera <c.coltrera@gmail.com>
2015-08-10 10:41:38 -0700

    Remove setup test, and repeat  creation.

--- test/karma_tests/blog_controller_test.js
+++ test/karma_tests/blog_controller_test.js
@@ -14,17 +14,9 @@ describe("blog controller", function() {
     $ControllerConstructor = _$controller_;
   }));
 
-  it("should be able to create a controller", function() {
-    var entriesController = $ControllerConstructor("blogController", { $scope: $scope});
-    expect(typeof entriesController).toBe("object");
-    expect(typeof $scope.getAll).toBe("function");
-    expect(Array.isArray($scope.entries)).toBe(true);
-  });
-
   describe("REST functionality", function() {
     beforeEach(angular.mock.inject(function(_$httpBackend_, _$rootScope_) {
       $httpBackend = _$httpBackend_;
-      $scope = _$rootScope_.$new();
       $ControllerConstructor("blogController", {$scope: $scope});
     }));
 
@@ -33,20 +25,24 @@ describe("blog controller", function() {
       $httpBackend.verifyNoOutstandingRequest();
     });
 
-  it("should make a get request when getAll is called", function() {
-      $httpBackend.expectGET("/api/entries").respond(200, [{
-        datePosted: new Date,
-        title: "what a title!",
-        entryBody: "what an entry!",
-        votes: 0,
-        _id: 1
-      }]);
-      $scope.getAll();
-      $httpBackend.flush();
-      expect($scope.entries.length).toBe(1);
-      expect($scope.entries[0].title).toBe("what a title!");
-      expect($scope.entries[0]._id).toBe(1);
+    it("should make a get request when getAll is called", function() {
+        $httpBackend.expectGET("/api/entries").respond(200, [{
+          datePosted: new Date,
+          title: "what a title!",
+          entryBody: "what an entry!",
+          votes: 0,
+          _id: 1
+        }]);
+        $scope.getAll();
+        $httpBackend.flush();
+        expect($scope.entries.length).toBe(1);
+        expect($scope.entries[0].title).toBe("what a title!");
+        expect($scope.entries[0]._id).toBe(1);
+    });
+
   });
+
+  describe("Non-REST functionality", function() {
 
   });
 
--- test/karma_tests/test_bundle.js
+++ test/karma_tests/test_bundle.js
@@ -69,17 +69,9 @@
 	    $ControllerConstructor = _$controller_;
 	  }));
 
-	  it("should be able to create a controller", function() {
-	    var entriesController = $ControllerConstructor("blogController", { $scope: $scope});
-	    expect(typeof entriesController).toBe("object");
-	    expect(typeof $scope.getAll).toBe("function");
-	    expect(Array.isArray($scope.entries)).toBe(true);
-	  });
-
 	  describe("REST functionality", function() {
 	    beforeEach(angular.mock.inject(function(_$httpBackend_, _$rootScope_) {
 	      $httpBackend = _$httpBackend_;
-	      $scope = _$rootScope_.$new();
 	      $ControllerConstructor("blogController", {$scope: $scope});
 	    }));
 
@@ -88,20 +80,24 @@
 	      $httpBackend.verifyNoOutstandingRequest();
 	    });
 
-	  it("should make a get request when getAll is called", function() {
-	      $httpBackend.expectGET("/api/entries").respond(200, [{
-	        datePosted: new Date,
-	        title: "what a title!",
-	        entryBody: "what an entry!",
-	        votes: 0,
-	        _id: 1
-	      }]);
-	      $scope.getAll();
-	      $httpBackend.flush();
-	      expect($scope.entries.length).toBe(1);
-	      expect($scope.entries[0].title).toBe("what a title!");
-	      expect($scope.entries[0]._id).toBe(1);
+	    it("should make a get request when getAll is called", function() {
+	        $httpBackend.expectGET("/api/entries").respond(200, [{
+	          datePosted: new Date,
+	          title: "what a title!",
+	          entryBody: "what an entry!",
+	          votes: 0,
+	          _id: 1
+	        }]);
+	        $scope.getAll();
+	        $httpBackend.flush();
+	        expect($scope.entries.length).toBe(1);
+	        expect($scope.entries[0].title).toBe("what a title!");
+	        expect($scope.entries[0]._id).toBe(1);
+	    });
+
 	  });
+
+	  describe("Non-REST functionality", function() {
 
 	  });
 
@@ -28599,7 +28595,7 @@
 	      $http.post("/api/entries", entry)
 	        .then(function(res) {
 	          // success
-	          $scope.entries.push(res.data);
+	          $scope.entries.unshift(res.data);
 	        }, function(res) {
 	          // error
 	          errorHandler(res);
