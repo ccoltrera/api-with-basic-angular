e4c04606ebf6c459a55ac2973c8e8b4627ba37fd
Colin Coltrera <c.coltrera@gmail.com>
2015-08-13 13:03:41 -0700

    Make blog form directive, switch out forms in the views with use of the directive.

--- app/js/blog/directives/directives.js
+++ app/js/blog/directives/directives.js
@@ -1,4 +1,4 @@
 "use strict";
 
 var directives = angular.module("directives", []);
-require("./blog_writer_directive")(directives);
+require("./entry_writer_directive")(directives);
---
+++ app/js/blog/directives/entry_writer_directive.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function(app) {
+  app.directive("entryWriter", function() {
+    return {
+      restrict: "AC",
+      replace: true,
+      templateUrl: "/js/blog/templates/entry_writer_template.html",
+      scope: {
+        save: "&",
+        name: "=",
+        buttonText: "=",
+        entry: "="
+      }
+    }
+  })
+};
